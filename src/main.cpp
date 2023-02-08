28d575b183182efba91133d2f26014a32bda2749
Yodale1000 <yodale@tutanota.com>
2023-02-08 14:46:12 +0100

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,43 @@
 #include <WiFiMulti.h>
-WiFiMulti wifiMulti;
-#define DEVICE "ESP32"
-
 #include <Adafruit_Sensor.h>
 #include <Adafruit_SHT31.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_DotStar.h>
-
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
 
+#define DEVICE "ESP32"
 #define WIFI_SSID "Student"
 #define WIFI_PASSWORD "Kristiania1914"
 #define INFLUXDB_URL "https://influxdb.guardiansofentity.de/"
 #define INFLUXDB_TOKEN "DP8vStYHSSQMKMtrfzUKMPED8Culx0wKsXPs7LVZ0tKdEiveXjL5rj9RMZc9kP0NC0iV25MRrCWaihieiT-xoQ=="
 #define INFLUXDB_ORG "Tropiske frukter Norge"
 #define INFLUXDB_BUCKET "Sensor_Data"
-
 #define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
-
 #define NUMPIXELS 1
 #define DATAPIN    33
 #define CLOCKPIN   21
-
 #define CLICKTHRESHHOLD 80
 
+WiFiMulti wifiMulti;
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-
-Point sensor("climate");
+Point climate("climate");
 Point motion("motion");
-
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
-
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
-
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
 void setup() {
   Serial.begin(115200);
-
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  climate.addTag("device", DEVICE);
+  motion.addTag("device", DEVICE);
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+  strip.begin();            
+  strip.setBrightness(20);
+  strip.setPixelColor(0,255,0,0);
+  strip.show();   
 
   Serial.println("Connecting to wifi");
   while (wifiMulti.run() != WL_CONNECTED) {
@@ -49,18 +46,12 @@ void setup() {
   }
   Serial.println();
 
-  sensor.addTag("device", DEVICE);
-  motion.addTag("device", DEVICE);
-
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-
-  if (client.validateConnection()) {
-    Serial.println("Connected to InfluxDB: ");
-    Serial.println(client.getServerUrl());
-  } else {
+  if (!client.validateConnection()) {
     Serial.println("InfluxDB connection failed: ");
     Serial.println(client.getLastErrorMessage());
+    while (1) delay(1);
   }
+  Serial.println("Connected to InfluxDB: ");
 
   if (! sht31.begin(0x44)) {
     Serial.println("Couldn't find SHT31 sensor!");
@@ -70,86 +61,43 @@ void setup() {
 
   if (! lis.begin(0x18)) {
     Serial.println("Couldn't find LIS3DH sensor!");
-    while (1) yield();
+    while (1) delay(1);
   }
   Serial.println("LIS3DH found!");
-
-  strip.begin();            
-  strip.setBrightness(20);
-  strip.setPixelColor(0,255,0,0);
-  strip.show();   
-}
-
-void error() {
-  strip.setPixelColor(0,0,255,0);
-  strip.show();   
-}
-
-void success() {
-  strip.setPixelColor(0,0,0,255);
-  strip.show();   
 }
 
 void loop() {
   strip.setPixelColor(0,255,0,0);
   strip.show();   
-
-  sensor.clearFields();
+  climate.clearFields();
   motion.clearFields();
-
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
-
+  climate.addField("temperature", t);
+  climate.addField("humidity", h);
   lis.read();
-
-  sensor.addField("humidity", h);
-  sensor.addField("temperature", t);
   motion.addField("X", lis.x);
   motion.addField("Y", lis.y);
   motion.addField("Z", lis.z);
 
-  if (! isnan(t)) {
-    Serial.print("Temp *C : "); Serial.print(t);
-    Serial.println();
-  } else { 
-    Serial.println("Failed to read temperature");
-    error();
-  }
-  
-  if (! isnan(h)) {
-    Serial.print("Hum. % : "); Serial.print(h);
-    Serial.println();
-  } else { 
-    Serial.println("Failed to read humidity");
-    error();
-  }
-
-  lis.read();
-  Serial.print("X: "); Serial.println(lis.x);
-  Serial.print("Y: "); Serial.println(lis.y);
-  Serial.print("Z: "); Serial.println(lis.z);
-
-  Serial.print("Writing: ");
-  Serial.println(sensor.toLineProtocol());
-
   if ((WiFi.RSSI() == 0) && (wifiMulti.run() != WL_CONNECTED)) {
     Serial.println("Wifi connection lost");
-    error();
+    strip.setPixelColor(0,0,255,0);
+    strip.show();   
   }
 
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB sensor write failed: ");
-    Serial.println(client.getLastErrorMessage());
-    error();
-  }
+  Serial.println("Writing: ");
+  Serial.println(climate.toLineProtocol());
+  Serial.println(motion.toLineProtocol());
 
-  if (!client.writePoint(motion)) {
-    Serial.print("InfluxDB motion write failed: ");
+  if (!client.writePoint(climate) || !client.writePoint(motion)) {
+    Serial.print("InfluxDB write failed: ");
     Serial.println(client.getLastErrorMessage());
-    error();
+    strip.setPixelColor(0,0,255,0);
+    strip.show();   
   }
   
-  success();
-  Serial.println("Wait 1s");
+  strip.setPixelColor(0,0,0,255);
+  strip.show(); 
   delay(1000);
 }
