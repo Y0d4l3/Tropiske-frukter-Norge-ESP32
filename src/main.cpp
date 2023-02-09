69f7740b4933de38356005944f2ef622c5855275
Yodale1000 <yodale@tutanota.com>
2023-02-09 20:30:28 +0100

    added secrets.h

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,7 @@
 #include <Adafruit_DotStar.h>
 #include <InfluxDbClient.h>
 #include <InfluxDbCloud.h>
-
-#define DEVICE "ESP32"
-#define WIFI_SSID "Student"
-#define WIFI_PASSWORD "Kristiania1914"
-#define INFLUXDB_URL "https://influxdb.guardiansofentity.de/"
-#define INFLUXDB_TOKEN "DP8vStYHSSQMKMtrfzUKMPED8Culx0wKsXPs7LVZ0tKdEiveXjL5rj9RMZc9kP0NC0iV25MRrCWaihieiT-xoQ=="
-#define INFLUXDB_ORG "Tropiske frukter Norge"
-#define INFLUXDB_BUCKET "Sensor_Data"
-#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
-#define NUMPIXELS 1
-#define DATAPIN    33
-#define CLOCKPIN   21
-#define CLICKTHRESHHOLD 80
+#include <secrets.h>
 
 WiFiMulti wifiMulti;
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
