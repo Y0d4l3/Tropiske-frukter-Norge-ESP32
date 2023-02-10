0a3887c0af6b74e5457e3a705c83683025c8b19d
Yodale1000 <yodale@tutanota.com>
2023-02-10 19:06:23 +0100

    improved privacy ;/

--- include/secrets.h
+++ include/secrets.h
@@ -1,12 +1,12 @@
 
-#define DEVICE "ESP32"
-#define WIFI_SSID "Student"
-#define WIFI_PASSWORD "Kristiania1914"
-#define INFLUXDB_URL "https://influxdb.guardiansofentity.de/"
-#define INFLUXDB_TOKEN "DP8vStYHSSQMKMtrfzUKMPED8Culx0wKsXPs7LVZ0tKdEiveXjL5rj9RMZc9kP0NC0iV25MRrCWaihieiT-xoQ=="
-#define INFLUXDB_ORG "Tropiske frukter Norge"
-#define INFLUXDB_BUCKET "Sensor_Data"
-#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
+#define DEVICE ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define INFLUXDB_URL ""
+#define INFLUXDB_TOKEN ""
+#define INFLUXDB_ORG ""
+#define INFLUXDB_BUCKET ""
+#define TZ_INFO ""
 #define NUMPIXELS 1
 #define DATAPIN    33
 #define CLOCKPIN   21
